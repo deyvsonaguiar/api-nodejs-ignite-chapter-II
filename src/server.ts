7d1e81f9111bd3bf876f41d600552d4b44090ae2
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-11-08 11:47:40 -0400

    Config ts-node-dev to reaload changes

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express()
+
+app.listen(3333, () => console.log("Server is running..."))
+
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World!" })
+})
+
