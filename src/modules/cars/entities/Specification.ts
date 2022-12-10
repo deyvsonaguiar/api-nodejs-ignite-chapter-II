a2d3b30165ab698c4ea8c03942883d6164453fd2
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-12-10 19:52:59 -0400

    Creating migrations to Specifications

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,10 +1,17 @@
 import { v4 as uuid } from 'uuid'
+import { Column, Entity, PrimaryColumn } from 'typeorm'
+import { CreateDateColumn } from 'typeorm';
 
+@Entity("specifications")
 class Specification {
 
+    @PrimaryColumn()
     id?: string
+    @Column()
     name: string
+    @Column()
     description: string
+    @CreateDateColumn()
     created_at: Date
 
     constructor() {
