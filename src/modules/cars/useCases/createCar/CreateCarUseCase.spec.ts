3d1583508eebacb39cb22f47b0ca3b9943eab178
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2023-04-17 11:16:02 -0400

    TDD - Finishing tests Create Car

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -13,7 +13,7 @@ describe("Create car", () => {
     })
 
     it("should be able to create a new car", async () => {
-        await createCarUseCase.execute({
+        const car = await createCarUseCase.execute({
             name: "Name car", 
             description: "Description car", 
             daily_rate: 100, 
@@ -22,6 +22,7 @@ describe("Create car", () => {
             brand: "Brand",
             category_id: "category"
         })
+        expect(car).toHaveProperty("id")
     })
 
     it("should not be able to create a car with exists license plate", () => {
